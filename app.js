5823a39635d556f9d473b18ecd96c6413aaf7feb
Fabio M. Gonçalves <167380233+FabioMGonc@users.noreply.github.com>
2025-04-08 22:54:01 -0300

    feat(agenda): estrutura inicial com HTML, CSS base e manipulação de elementos via JS

---
+++ app.js
@@ -0,0 +1,4 @@
+const titleHeader = document.getElementById('title-header');
+const newContact = document.getElementById('newContact');
+const searchContact = document.getElementById('searchContact');
+const manageContact = document.getElementById('manageContact')

7e18663dc409d29261d36d1661411837d1a4768f
Fabio M. Gonçalves <167380233+FabioMGonc@users.noreply.github.com>
2025-04-10 23:21:09 -0300

    feat(agenda): implementa layout base,criação da pasta pages, navegação via iframe e alguns ajustes no css

--- app.js
+++ app.js
@@ -1,4 +1,34 @@
 const titleHeader = document.getElementById('title-header');
 const newContact = document.getElementById('newContact');
 const searchContact = document.getElementById('searchContact');
-const manageContact = document.getElementById('manageContact')
+const manageContact = document.getElementById('manageContact');
+const buttonIndex = document.getElementById('index');
+const iframe = document.querySelector('iframe')
+
+
+newContact.addEventListener('click', (event) => {
+    event.preventDefault()
+    selectTab(event.target);
+    window.open("/pages/novoContato.html", "body-contains")
+});
+
+searchContact.addEventListener('click', (event) => {
+    event.preventDefault()
+    selectTab(event.target);
+    window.open("/pages/pesquisar.html", "body-contains")
+});
+
+manageContact.addEventListener('click', (event) => {
+    event.preventDefault()
+    selectTab(event.target);
+    window.open("/pages/gerirContatos.html", "body-contains")
+});
+
+const selectTab = (element) => {
+    
+    const tabs = [...document.querySelectorAll('.tabs')];
+    tabs.forEach((e) => {
+        e.classList.remove('tabsSelected');
+    })
+    element.classList.add('tabsSelected');
+}
